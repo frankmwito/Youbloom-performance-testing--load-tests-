115224faea98d63727875e6c55913212cfff1096
Frank Mwito <Mfranklyne039@gmail.com>
2025-03-12 12:46:25 +0300

    High transactions load tests

---
+++ performance-tests/High_Transactions.py
@@ -0,0 +1,4 @@
+# Contains the tests for high transactions critical user flows for the application under test (AUT) 
+# sign up, login, logout, account management, search, add to cart, checkout, etc.
+
+
